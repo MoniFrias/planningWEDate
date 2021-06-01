766b945095050d7e500a6d9efceed1e7d4472419
Jason <jason.limon@outlook.com>
2021-06-01 13:31:50 -0500

    Adding response 400

--- planningWEDate-service/src/main/java/com/example/planningWEDate/services/Services.java
+++ planningWEDate-service/src/main/java/com/example/planningWEDate/services/Services.java
@@ -3,23 +3,31 @@ package com.example.planningWEDate.services;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.example.planningWEDate.entity.DateWE;
 import com.example.planningWEDate.entity.Response;
 import com.example.planningWEDate.entity.WEDateBeforeTodayException;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class Services {
 
+	private Logger log = LoggerFactory.getLogger(Services.class);
+
 	public Response save(LocalDate dateWe) {
+		log.info("Starting method save {}", dateWe);
 		Response response = new Response();
 		if(dateWe.isAfter(LocalDate.now())|| dateWe.isEqual(LocalDate.now()) ) {
 			LocalDate PublishedWEDate = setWeekEndingDate(dateWe.minusDays(7));
+			log.info(" PublishedWEDate is {} ", PublishedWEDate);
 			response.setData(PublishedWEDate);
 			return response;
 		}
-		throw new WEDateBeforeTodayException();
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("Bad Request with the next date %s", dateWe));
 	}
 	
 	
